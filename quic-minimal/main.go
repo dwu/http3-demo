38fbde0a29b3456fcd15bf71327c8b3fc7b17682
Daniel Wutke <dwu.public@gmail.com>
2023-06-10 07:19:30 +0200

    Add quic-minimal
    Based on https://github.com/quic-go/quic-go/blob/master/example/echo/echo.go

---
+++ quic-minimal/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"log"
+	"math/big"
+
+	"github.com/quic-go/quic-go"
+)
+
+const addr = "localhost:4242"
+
+func main() {
+	go func() { log.Fatal(echoServer()) }()
+
+	err := clientMain()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func echoServer() error {
+	listener, err := quic.ListenAddr(addr, generateTLSConfig(), nil)
+	if err != nil {
+		return err
+	}
+	conn, err := listener.Accept(context.Background())
+	if err != nil {
+		return err
+	}
+	stream, err := conn.AcceptStream(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(loggingWriter{stream}, stream)
+	return err
+}
+
+func clientMain() error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	conn, err := quic.DialAddr(context.Background(), addr, tlsConf, nil)
+	if err != nil {
+		return err
+	}
+
+	stream, err := conn.OpenStreamSync(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < 3; i += 1 {
+		message := fmt.Sprintf("response %d", i)
+		fmt.Printf("Client: Sending '%s'\n", message)
+		_, err = stream.Write([]byte(message))
+		if err != nil {
+			return err
+		}
+
+		buf := make([]byte, len(message))
+		_, err = io.ReadFull(stream, buf)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Client: Got '%s'\n", buf)
+	}
+
+	return nil
+}
+
+type loggingWriter struct{ io.Writer }
+
+func (w loggingWriter) Write(b []byte) (int, error) {
+	fmt.Printf("Server: Got '%s'\n", string(b))
+	return w.Writer.Write(b)
+}
+
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
+	}
+}
